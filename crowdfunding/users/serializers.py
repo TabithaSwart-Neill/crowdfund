7c562fce3627a8558b0acca4fd69329d088bcc07
TabithaSwart-Neill <tabithaswart@icloud.com>
2020-09-19 10:25:05 +1000

    Saturday Class 19 Sep

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -8,3 +8,10 @@ class CustomUserSerializer(serializers.Serializer):
     password = serializers.CharField(write_only = True)
     def create(self, validated_data):
         return CustomUser.objects.create_user(**validated_data)
+
+class UserDetailSerializer(CustomUserSerializer):
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+        return instance
