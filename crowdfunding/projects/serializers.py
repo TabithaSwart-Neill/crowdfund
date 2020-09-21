86810442c0dac6056f7ac97c3cbe26c72ff66c99
TabithaSwart-Neill <tabithaswart@icloud.com>
2020-09-21 19:28:41 +1000

    Update Pledges
    Update Pledges and View Single Pledge

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -1,15 +1,6 @@
 from rest_framework import serializers
 from .models import Project, Pledge
-class PledgeSerializer(serializers.Serializer):
-    id = serializers.ReadOnlyField()
-    amount = serializers.IntegerField()
-    comment = serializers.CharField(max_length=200)
-    anonymous = serializers.BooleanField()
-    supporter_id = serializers.IntegerField()
-    # supporter = serializers.CharField(max_length=200)
-    project_id = serializers.IntegerField()
-    def create(self, validated_data):
-        return Pledge.objects.create(**validated_data)
+
 class ProjectSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     title = serializers.CharField(max_length=200)
@@ -18,13 +9,12 @@ class ProjectSerializer(serializers.Serializer):
     image = serializers.URLField()
     is_open = serializers.BooleanField()
     date_created = serializers.DateTimeField()
-    # owner = serializers.CharField(max_length=200)
     owner = serializers.ReadOnlyField(source='owner.id')
-    # pledges = PledgeSerializer(many=True, read_only=True)
     def create(self, validated_data):
         return Project.objects.create(**validated_data)
+
 class ProjectDetailSerializer(ProjectSerializer):
-    pledges = PledgeSerializer(many=True, read_only=True)
+    # pledges = PledgeSerializer(many=True, read_only=True)
     def update(self, instance, validated_data):
         instance.title = validated_data.get('title', instance.title)
         instance.description = validated_data.get('description',instance.description)
@@ -35,3 +25,27 @@ class ProjectDetailSerializer(ProjectSerializer):
         instance.owner = validated_data.get('owner', instance.owner)
         instance.save()
         return instance
+
+class PledgeSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    amount = serializers.IntegerField()
+    comment = serializers.CharField(max_length=200)
+    anonymous = serializers.BooleanField()
+    supporter_id = serializers.IntegerField()
+    # supporter = serializers.CharField(max_length=200)
+    project_id = serializers.IntegerField()
+    def create(self, validated_data):
+        return Pledge.objects.create(**validated_data)
+
+class PledgeDetailSerializer(PledgeSerializer):
+    def update(self, instance, validated_data):
+        instance.amount = validated_data.get('amount', instance.amount)
+        instance.comment = validated_data.get('comment',instance.comment)
+        instance.anonymous = validated_data.get('anonymous', instance.anonymous)
+        instance.project_id = validated_data.get('project_id', instance.project_id)
+        instance.supporter_id = validated_data.get('supporter_id', instance.supporter_id)
+        instance.save()
+        return instance
+
+
+
--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-# Create your views here.
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Project, Pledge
-from .serializers import ProjectSerializer, PledgeSerializer,ProjectDetailSerializer
+from .serializers import ProjectSerializer, PledgeSerializer,ProjectDetailSerializer, PledgeDetailSerializer
 from django.http import Http404
 from rest_framework import status, permissions
-from .permissions import IsOwnerOrReadOnly
+from .permissions import IsOwnerOrReadOnly, IsSupporterOrReadOnly
+
 class ProjectList(APIView):
     # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     def get(self, request):
@@ -36,13 +36,13 @@ class ProjectDetail(APIView):
             return project
         except Project.DoesNotExist:
             raise Http404
+
     def get(self, request, pk):
         project = self.get_object(pk)
-        # serializer = ProjectSerializer(project)
         serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
+
     def put(self, request, pk):
-        # it is for update 
         project = self.get_object(pk)
         data = request.data
         serializer = ProjectDetailSerializer(instance=project, data=data, partial=True)
@@ -56,6 +56,7 @@ class PledgeList(APIView):
         pledges = Pledge.objects.all()
         serializer = PledgeSerializer(pledges, many=True)
         return Response(serializer.data)
+
     def post(self, request):
         serializer = PledgeSerializer(data=request.data)
         if serializer.is_valid():
@@ -68,3 +69,34 @@ class PledgeList(APIView):
         serializer.errors,
         status=status.HTTP_400_BAD_REQUEST
         )
+
+class PledgeDetailList(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsSupporterOrReadOnly
+    ]
+    def get_object(self, pk):
+        try:
+            pledge = Pledge.objects.get(pk=pk)
+            self.check_object_permissions(self.request, pledge)
+            return pledge
+        except Pledge.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        pledge = self.get_object(pk)
+        serializer = PledgeDetailSerializer(pledge)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        pledge = self.get_object(pk)
+        data = request.data
+        serializer = PledgeDetailSerializer(
+            instance=pledge, 
+            data=data, 
+            partial=True
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
