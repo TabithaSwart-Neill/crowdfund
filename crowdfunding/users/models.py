bbb6662d679936042e794539e54726d813b62880
TabithaSwart-Neill <tabithaswart@icloud.com>
2020-09-26 08:59:42 +1000

    User fields added

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    pass
-
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50,default='')
+    # profile_pic = models.ImageField(upload_to='images/', null=True, blank=True)
 
     def __str__(self):
         return self.username
